254aa1e48723a41f36bf4d0e0a93783489c60cf2
Thiago Bastos dos Santos <bthiagos@gmail.com>
2022-06-22 15:45:50 -0300

    Update MovimentacaoService.java
    Versão Final

--- src/main/java/com/dio/santander/bankline/api/service/MovimentacaoService.java
+++ src/main/java/com/dio/santander/bankline/api/service/MovimentacaoService.java
@@ -14,6 +14,7 @@ import com.dio.santander.bankline.api.repository.MovimentacaoRepository;
 
 @Service
 public class MovimentacaoService {
+	
 	@Autowired
 	private MovimentacaoRepository repository;
 	
